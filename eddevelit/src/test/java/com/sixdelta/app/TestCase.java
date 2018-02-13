7e13f22d3a87d7b0e241b20a3051e09ad35d9b45
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-13 14:29:09 -0600

    Antes de prueba 3

--- eddevelit/src/test/java/com/sixdelta/app/TestCase.java
+++ eddevelit/src/test/java/com/sixdelta/app/TestCase.java
@@ -25,7 +25,7 @@ public class TestCase {
     assertEquals(n,89);
   }
 
-    /*
+
   @Test
   public void testFibonnaccSequence(){
     Fibonacci fibonacci = new Fibonacci();
@@ -41,6 +41,6 @@ public class TestCase {
     assertEquals(n,44);
     System.out.println(fibonacci.getTheSumOfEvenNumbersUntil(4000000));
   }
-*/
+
 
 }
