62b27490b8935ee977fd64f3a5108a48d0b1fe25
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-02-13 16:05:55 -0600

    Un refactor de StringCalculator

--- AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
+++ AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -5,18 +5,27 @@ public class StringCalculator {
 	public int add(String numbers) {
 		if (numbers.isEmpty())
 			return 0;
-		else if(numbers.contains(",")) {
+		
+		if(isSingleNumber(numbers)) {
 			String[] numeros_a_sumar = numbers.split(",");
 			return Integer.sum(convertToInt(numeros_a_sumar[0]), convertToInt(numeros_a_sumar[1]));
 		}
-		else
-			return Integer.parseInt(numbers);
+		
+		return Integer.parseInt(numbers);
 		
 		
 	}
 	
 	public int convertToInt(String cadena) {
 		return Integer.parseInt(cadena);
+	}
+	
+	private boolean isSingleNumber(String number) {
+		return isMultipleNumber(number);
+	}
+	
+	private boolean isMultipleNumber(String number) {
+		return number.split(",").length > 1;
 	}
 	
 	
