b80d35eb53bcd0a67bd77d41fbbe2984e1b51f66
neodevelop <juan@makingdevs.com>
2018-02-13 12:52:41 -0600

    Obteniendo los Fibo de N

--- neodevelop/src/main/java/com/sixdelta/app/Fibonacci.java
+++ neodevelop/src/main/java/com/sixdelta/app/Fibonacci.java
@@ -3,8 +3,14 @@ package com.sixdelta.app;
 public class Fibonacci {
 
   public int getForPosition(int n){
-    return 0;
+    return calculateFibonacci(n);
   }
+
+  private int calculateFibonacci(int n){
+    if(n < 2) return 1;
+    return calculateFibonacci(n-1) + calculateFibonacci(n - 2);
+  }
+
   public int[] getSequenceUntil(int n){
     int[] sequence = {1,2,3,4,5};
     return sequence;
