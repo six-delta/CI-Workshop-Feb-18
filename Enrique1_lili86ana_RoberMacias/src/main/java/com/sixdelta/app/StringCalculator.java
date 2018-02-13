dbe01727170460543e3db034503926ab143189a9
Roberto <roberto.macias@apimicroservicios.com>
2018-02-13 16:25:19 -0600

    Prueba sumando dos numeros

--- Enrique1_lili86ana_RoberMacias/src/main/java/com/sixdelta/app/StringCalculator.java
+++ Enrique1_lili86ana_RoberMacias/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -3,7 +3,26 @@ package com.sixdelta.app;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		if(numbers.isEmpty()) return 0;
-		else return Integer.parseInt(numbers);
+		if (numbers.isEmpty())
+			return 0;
+		else if (containChar(numbers, ",")) {
+			return sumString(numbers);
+		}
+		return Integer.parseInt(numbers);
+
+	}
+	
+	private boolean containChar(String cadena, String charContains) {
+		if (cadena.indexOf(charContains) == 1) return true;
+		return false;		
+	}
+
+	private  int sumString(String numbers) {
+		String[] numero = numbers.split(",");
+		int sumNum = 0;
+		for (int i = 0; i < numero.length; i++) {
+			sumNum += Integer.parseInt(numero[i]);
+		}
+		return sumNum;
 	}
 }
