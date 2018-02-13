aaa9c62643daccbc1c5b1d67300468a76724d370
neodevelop <juan@makingdevs.com>
2018-02-13 12:41:01 -0600

    Agregando clase Fibonacci

---
+++ neodevelop/src/main/java/com/sixdelta/app/Fibonacci.java
@@ -0,0 +1,16 @@
+package com.sixdelta.app;
+
+public class Fibonacci {
+
+  public int getForPosition(int n){
+    return 0;
+  }
+  public int[] getSequenceUntil(int n){
+    int[] sequence = {1,2,3,4,5};
+    return sequence;
+  }
+
+  public int getTheSumOfEvenNumbersUntil(int n){
+    return 0;
+  }
+}
