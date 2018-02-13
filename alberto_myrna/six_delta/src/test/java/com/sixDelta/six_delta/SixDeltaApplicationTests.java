be252feed5a958c0cb1fe596104fd8b95170f8e8
albertojhs <alberto.jhs.33@gmail.com>
2018-02-13 15:21:24 -0600

    Vaciando archvios

--- alberto_myrna/six_delta/src/test/java/com/sixDelta/six_delta/SixDeltaApplicationTests.java
+++ alberto_myrna/six_delta/src/test/java/com/sixDelta/six_delta/SixDeltaApplicationTests.java
@@ -10,23 +10,4 @@ import com.sixDelta.six_delta.Fibonacci;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SixDeltaApplicationTests {
-
-	@Test
-	public void simple() {
-		assertEquals(1,1);
-
-	}
-
-
-	@Test
-	public void fibonacciDeUnNumeroDado(){
-		Fibonacci fibo = new Fibonacci();
-		assertEquals(3524578,fibo.calculaPara(32));
-	}
-
-	@Test
-	public void sumaDeNumeros(){
-		Fibonacci fibo = new Fibonacci();
-		assertEquals(4613732,fibo.calculaSumaDeNNumeros(32));
-	}
 }
