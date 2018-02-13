c7601133ec9d8e2bc74b44a14d58efaea2f169ab
souhain <edgarparedes283@gmail.com>
2018-02-13 11:02:43 -0600

    prueba 1

---
+++ souhain/src/test/java/com/sixdelta/app/TestUno.java
@@ -0,0 +1,13 @@
+package com.sixdelta.app;
+ 
+import org.junit.*;
+import static org.junit.Assert.*;
+ 
+public class TestUno {
+ 
+   @Test
+   public void firstTest(){
+     assertEquals(1,1);
+   }
+ 
+ }
