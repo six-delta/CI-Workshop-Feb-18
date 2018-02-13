cf55ec645cbf98f4c484d13437561e1d07299a86
neodevelop <juan@makingdevs.com>
2018-02-13 11:10:01 -0600

    Corrigiendo errores de compilación

--- neodevelop/src/test/java/com/sixdelta/app/TestCase.java
+++ neodevelop/src/test/java/com/sixdelta/app/TestCase.java
@@ -12,30 +12,30 @@ public class TestCase {
 
   @Test
   public void testFibonnacciWithOneNumber(){
-    Fibonacci fibonnacci = new Fibonacci();
-    int n = fibonnaci.getForPosition(3);
+    Fibonacci fibonacci = new Fibonacci();
+    int n = fibonacci.getForPosition(3);
     assertEquals(n,3);
-    n = fibonnaci.getForPosition(4);
+    n = fibonacci.getForPosition(4);
     assertEquals(n,5);
-    n = fibonnaci.getForPosition(5);
+    n = fibonacci.getForPosition(5);
     assertEquals(n,8);
-    n = fibonnaci.getForPosition(6);
+    n = fibonacci.getForPosition(6);
     assertEquals(n,13);
-    n = fibonnaci.getForPosition(10);
+    n = fibonacci.getForPosition(10);
     assertEquals(n,89);
   }
 
   @Test
   public void testFibonnaccSequence(){
-    Fibonacci fibonnacci = new Fibonacci();
-    int[] result = Fibonacci.getSequenceUntil(10);
+    Fibonacci fibonacci = new Fibonacci();
+    int[] result = fibonacci.getSequenceUntil(10);
     int[] expectedResult = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89} ;
     assertArrayEquals( expectedResult, result );
   }
 
   @Test
   public void testSumFibonnacciEvenNumbers(){
-    Fibonacci fibonnacci = new Fibonacci();
+    Fibonacci fibonacci = new Fibonacci();
     int n = fibonacci.getTheSumOfEvenNumbersUntil(10);
     assertEquals(n,44);
     System.out.println(fibonacci.getTheSumOfEvenNumbersUntil(4000000));
