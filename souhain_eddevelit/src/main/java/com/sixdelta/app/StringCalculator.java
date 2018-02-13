d184fa39b9b023d21055dd42b6106fdc79dc2ef4
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-13 17:09:59 -0600

    Dos pruebas completas

--- souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
+++ souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -3,7 +3,15 @@ package com.sixdelta.app;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		if(numbers.isEmpty()) return 0;
-		else return Integer.parseInt(numbers);
+		
+		if(numbers.isEmpty()) { return 0;
+		}
+		
+		else {
+	    String[] parts = numbers.split(",");
+		String part1 = parts[1];
+		String part2 = parts[2];
+			return Integer.parseInt(part1+part2);
+		}
 	}
 }
