d58e07738ff670a0d4f227c1785c4e5ba50fe38a
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-02-13 15:58:42 -0600

    Agregando prueba con dos números

--- AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
+++ AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -5,7 +5,20 @@ public class StringCalculator {
 	public int add(String numbers) {
 		if (numbers.isEmpty())
 			return 0;
+		else if(numbers.contains(",")) {
+			String[] numeros_a_sumar = numbers.split(",");
+			return Integer.sum(convertToInt(numeros_a_sumar[0]), convertToInt(numeros_a_sumar[1]));
+		}
 		else
 			return Integer.parseInt(numbers);
+		
+		
 	}
+	
+	public int convertToInt(String cadena) {
+		return Integer.parseInt(cadena);
+	}
+	
+	
+	
 }
