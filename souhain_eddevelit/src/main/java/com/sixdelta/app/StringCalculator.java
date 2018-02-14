0553ef24f66f13c4633ec934d49b7ecb74a71943
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-14 16:15:24 -0600

    Priemeras 5 pruebas aprobadas

--- souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
+++ souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -3,29 +3,30 @@ package com.sixdelta.app;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		
-	
 
+		
+		//prueba 1 
 		if (numbers.isEmpty()) {
 			return 0;
 		}
 
+		//Evalua si es tipo string
 		if (numbers.getClass().equals(String.class)) {
-
-			String contiene = numbers;
-			CharSequence cs1 = ",";
-			boolean retval = contiene.contains(cs1);
-			if (retval) {
-				String numero = numbers;
-				String[] partes = numero.split(",");
-				int numeroArray = 0;
-				for (int i = 0; i< partes.length; i++){
-			    numeroArray += Integer.parseInt(partes[i]) ;
-			 
-			        }
-				return numeroArray;
+			//Reemplaza los saltos de linea
+			String remplazo = numbers.replace("\n", ",");
+			//Tiene coma ?
+			CharSequence coma = ",";
+			boolean tieneComa = remplazo.contains(coma);
+			//
+			if (tieneComa) {
+				String[] partes = remplazo.split(",");
+				int sumaArray = 0;
+				for (int i = 0; i < partes.length; i++) {
+					sumaArray += Integer.parseInt(partes[i]);
+				}
+				return sumaArray;
 			}
-			
+
 		}
 		return Integer.parseInt(numbers);
 
--- souhain_eddevelit/src/test/java/com/sixdelta/app/StringCalculatorTests.java
+++ souhain_eddevelit/src/test/java/com/sixdelta/app/StringCalculatorTests.java
@@ -1,11 +1,16 @@
 package com.sixdelta.app;
 
 import org.junit.Test;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 import static org.junit.Assert.*;
 
 public class StringCalculatorTests {
+	
 
 	@Test
+	@Ignore
 	public void testEmptyString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		int res = stringCalculator.add("");
@@ -13,6 +18,7 @@ public class StringCalculatorTests {
 	}
 
 	@Test
+	@Ignore
 	public void testOneNumberInTheString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		int res = stringCalculator.add("1");
@@ -26,6 +32,7 @@ public class StringCalculatorTests {
 	}
 
 	@Test
+	@Ignore
 	public void testTwoNumbersInTheString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		int res = stringCalculator.add("1,2");
@@ -39,6 +46,7 @@ public class StringCalculatorTests {
 	}
 	
 	@Test
+	@Ignore
 	public void testMoreNumbersInTheString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		int res = stringCalculator.add("1,2,3");
@@ -50,5 +58,17 @@ public class StringCalculatorTests {
 		res = stringCalculator.add("5,6,7,8,9");
 		assertEquals(35, res);
 	}
+	
+	@Test
+	public void testWithBackLineNumberInTheString() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int res = stringCalculator.add("0,1\n2,3");
+		assertEquals(6,res);
+		res = stringCalculator.add("56,88,345\n7777,999,1");
+		assertEquals(9266,res);
+		res = stringCalculator.add("4,5\n6");
+		assertEquals(15, res);
+	}
+
 
 }
