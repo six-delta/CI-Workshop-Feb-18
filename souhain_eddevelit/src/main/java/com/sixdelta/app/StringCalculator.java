5734a2a91ae0c9eb357df40817bc81d0d7f3f037
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-15 10:32:37 -0600

    Quitando sentencias if que evaluan string y coma

--- souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
+++ souhain_eddevelit/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -7,19 +7,12 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		if (numbers.getClass().equals(String.class)) {
 			String replace = numbers.replace("\n", ",");
-			CharSequence comma = ",";
-			boolean isComma = replace.contains(comma);
-			if (isComma) {
 				String[] parts = replace.split(",");
 				int addArray = 0;
 				for (int i = 0; i < parts.length; i++) {
 					addArray += Integer.parseInt(parts[i]);
 				}
 				return addArray;
-			}
-		}
-		return Integer.parseInt(numbers);
 	}
 }
