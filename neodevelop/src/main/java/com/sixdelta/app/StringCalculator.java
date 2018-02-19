e5973b29c72276f3fc8ba0a1afdf371346d34c39
neodevelop <juan@makingdevs.com>
2018-02-19 14:49:23 -0600

    Porfin, haciéndolo pasar, uff...

--- neodevelop/src/main/java/com/sixdelta/app/StringCalculator.java
+++ neodevelop/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -1,9 +1,16 @@
 package com.sixdelta.app;
 
+import java.util.List;
+
 public class StringCalculator {
 
 	public int add(String numbers) {
 		if(numbers.isEmpty()) return 0;
-		else return Integer.parseInt(numbers);
+		String[] sequenceNumbers = numbers.split(",");
+    int acumm = 0;
+    for(String n : sequenceNumbers){
+      acumm += Integer.parseInt(n);
+    }
+    return  acumm;
 	}
 }
