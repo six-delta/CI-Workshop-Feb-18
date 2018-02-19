c9c40a62b69a1dbace8f395936249f69da9a6b78
deenyA <deenyaguilar713@gmail.com>
2018-02-19 14:09:48 -0600

    Creando tqabla correctamente

--- DeenyAH/DeenyAH/src/main/java/com/sixdelta/DeenyAH/DeenyAhApplication.java
+++ DeenyAH/DeenyAH/src/main/java/com/sixdelta/DeenyAH/DeenyAhApplication.java
@@ -1,27 +1,15 @@
 package com.sixdelta.DeenyAH;
 
-import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.annotation.Id;
+
 
 @SpringBootApplication
 public class DeenyAhApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeenyAhApplication.class, args);
-	
-		
-	
-		
-	
-	
-	
+
 	}
 }
