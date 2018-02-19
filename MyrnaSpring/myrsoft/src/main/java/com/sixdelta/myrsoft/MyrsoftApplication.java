d84e045ae243b75d4477981867cf0d9cc904be72
myrsoft <myrmartinez951@gmail.com>
2018-02-19 10:05:47 -0600

    agreando archivos

---
+++ MyrnaSpring/myrsoft/src/main/java/com/sixdelta/myrsoft/MyrsoftApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.myrsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrsoftApplication.class, args);
+	}
+}
