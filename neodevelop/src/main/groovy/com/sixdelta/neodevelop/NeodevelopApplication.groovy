7f1da215f11d444c9411f7efea2643ed1477189b
neodevelop <juan@makingdevs.com>
2018-02-19 09:42:22 -0600

    Agregando proyecto de Spring Boot

---
+++ neodevelop/src/main/groovy/com/sixdelta/neodevelop/NeodevelopApplication.groovy
@@ -0,0 +1,12 @@
+package com.sixdelta.neodevelop
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NeodevelopApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run NeodevelopApplication, args
+	}
+}
---
+++ neodevelop/src/test/groovy/com/sixdelta/neodevelop/NeodevelopApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.sixdelta.neodevelop
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class NeodevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
