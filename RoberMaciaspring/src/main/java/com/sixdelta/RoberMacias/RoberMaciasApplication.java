6c591825836743c879bbf4ae0b029ce86c215acc
Roberto <roberto.macias@apimicroservicios.com>
2018-02-19 09:44:24 -0600

    Agregando proyecto de Spring boot

---
+++ RoberMaciaspring/src/main/java/com/sixdelta/RoberMacias/RoberMaciasApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.RoberMacias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoberMaciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoberMaciasApplication.class, args);
+	}
+}
