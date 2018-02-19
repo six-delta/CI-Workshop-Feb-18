da7fcfbed9f2ef1c43734b7b2011976e2bb6d347
svhector <nostgrand@gmail.com>
2018-02-19 11:38:27 -0600

    Crando tabla Task

--- svHector/src/main/java/com/sixdelta/svHector/SvHectorApplication.java
+++ svHector/src/main/java/com/sixdelta/svHector/SvHectorApplication.java
@@ -8,5 +8,6 @@ public class SvHectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SvHectorApplication.class, args);
+			
 	}
 }
---
+++ svHector/src/main/java/model/Task.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "db_svhector", catalog = "dbTest01")
+	public class Task {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer id;
+
+		private String description;
+		private Date dataCreated;
+		private Date lastUpdate;
+		private boolean finished;
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public Date getDataCreated() {
+			return dataCreated;
+		}
+		public void setDataCreated(Date dataCreated) {
+			this.dataCreated = dataCreated;
+		}
+		public Date getLastUpdate() {
+			return lastUpdate;
+		}
+		public void setLastUpdate(Date lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+		public boolean isFinished() {
+			return finished;
+		}
+		public void setFinished(boolean finished) {
+			this.finished = finished;
+		}
+		
+		
+		
+	}
+
+
