09f109282d7cf849eb6cd9f6154ffd61a44815cb
Javier Ramirez <cabestro_jj@hotmail.com>
2018-02-19 14:55:41 -0600

    Ya con servicio arrancado

---
+++ JAVI/JJAvierRamirez2/src/main/java/com/sixdelta/JJavierRamirez/model/Task.java
@@ -0,0 +1,36 @@
+package com.sixdelta.JJavierRamirez.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private String description;
+	private Date dateCreated;
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+	
+}
