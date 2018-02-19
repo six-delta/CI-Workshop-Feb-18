2bb7418605c8823c172413585f3ffb18d5b3b014
Roberto <roberto.macias@apimicroservicios.com>
2018-02-19 12:25:47 -0600

    Agregando CRUD para servicios

---
+++ RoberMaciaspring/src/main/java/com/sixdelta/RoberMacias/repositories/TasksRepositories.java
@@ -0,0 +1,10 @@
+package com.sixdelta.RoberMacias.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.sixdelta.RoberMacias.model.Tasks;
+
+public interface TasksRepositories extends CrudRepository<Tasks, Integer> {
+	
+
+
+}
