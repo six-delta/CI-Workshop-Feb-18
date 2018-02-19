03a997ce7eae95a9f6108dad6f0132d88e9e9db3
svhector <nostgrand@gmail.com>
2018-02-19 09:50:57 -0600

    Haciendo Cambios

--- SvHector/src/main/java/com/sixdelta/app/StringCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.sixdelta.app;
-
-public class StringCalculator {
-	
-	public int add(String numbers) {
-		if (numbers.isEmpty()) return 0;
-		else return Integer.parseInt(numbers);
-	}
-}
--- SvHector_Deeny/src/main/java/com/sixdelta/app/StringCalculator.java
+++ SvHector_Deeny/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -17,5 +17,4 @@ public class StringCalculator {
 		}
 		return sumaString;
 	}
-
 }
