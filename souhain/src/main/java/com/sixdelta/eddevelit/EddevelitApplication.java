82c541c8d361f1b195b36b005b74517925348162
LuisChore <luis.vazquez.ch@hotmail.com>
2018-02-19 13:10:10 -0600

    curso spring heredado del bro

---
+++ souhain/src/main/java/com/sixdelta/eddevelit/EddevelitApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.eddevelit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddevelitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddevelitApplication.class, args);
+	}
+}
