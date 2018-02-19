7047e6014d2b5225fe1201a6167dfd92e2fd57cf
Javier Ramirez <cabestro_jj@hotmail.com>
2018-02-19 09:47:37 -0600

    Agregando proyecto

---
+++ JAVI/JJavierRamirez/src/main/java/com/sixdelta/JJavierRamirez/JJavierRamirezApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.JJavierRamirez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JJavierRamirezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JJavierRamirezApplication.class, args);
+	}
+}
