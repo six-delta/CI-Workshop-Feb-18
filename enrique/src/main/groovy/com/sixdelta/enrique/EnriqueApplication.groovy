b649bc974a93078e9599a2d8502d8b54181f2878
Enrique <kiqueyo847@gmail.com>
2018-02-19 09:50:16 -0600

    Iniciando proyecto de spring

---
+++ enrique/src/main/groovy/com/sixdelta/enrique/EnriqueApplication.groovy
@@ -0,0 +1,12 @@
+package com.sixdelta.enrique
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EnriqueApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run EnriqueApplication, args
+	}
+}
---
+++ enrique/src/test/groovy/com/sixdelta/enrique/EnriqueApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.sixdelta.enrique
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class EnriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
