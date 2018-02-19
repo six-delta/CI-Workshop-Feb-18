c07bfed5ca5f566f020261ff32e36791095e8198
hollowbk201 <heihollowbk201@gmail.com>
2018-02-19 10:09:39 -0600

    Se agrega branch hollowbk201_springboot y proyecto de Intlzr

---
+++ hollowbk201sb/hollowbk201/src/main/java/com/sixdelta/hollowbk201/Hollowbk201Application.java
@@ -0,0 +1,12 @@
+package com.sixdelta.hollowbk201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hollowbk201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hollowbk201Application.class, args);
+	}
+}
---
+++ hollowbk201sb/hollowbk201/src/test/java/com/sixdelta/hollowbk201/Hollowbk201ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixdelta.hollowbk201;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hollowbk201ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
