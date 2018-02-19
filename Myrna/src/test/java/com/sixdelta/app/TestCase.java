5fa31e7c8b15ea075efdd185618c07a6921e8f9a
myrsoft <myrmartinez951@gmail.com>
2018-02-19 09:52:01 -0600

    Agregando proyecto de Spring Boot

---
+++ Myrna/src/test/java/com/sixdelta/app/TestCase.java
@@ -0,0 +1,12 @@
+package com.sixdelta.app;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestCase {
+
+  @Test
+  public void firstTest(){
+    assertEquals(1,1);
+  }
+}
--- alberto_myrna/six_delta/src/main/java/com/sixDelta/six_delta/StringCalculator.java
+++ alberto_myrna/six_delta/src/main/java/com/sixDelta/six_delta/StringCalculator.java
@@ -1,8 +1,15 @@
 package com.sixDelta.six_delta;
 
 	public class StringCalculator{
-		public int add(String numbers) {
+			
+			
+			String[] numeros = string.split(",");
+			int a = Integer.parseInt(numeros[0]);
+			int b = Integer.parseInt(numeros[1]);				       
+			return a + b;
+				   
 			if(numbers.isEmpty()) return 0;
+			
 			else return Integer.parseInt(numbers);
 		}
-}
+}	
