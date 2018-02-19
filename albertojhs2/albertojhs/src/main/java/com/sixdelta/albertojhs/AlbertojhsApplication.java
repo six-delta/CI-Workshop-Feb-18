3515c8ccc55c5d0ed054bb29a95637e806ceca56
albertojhs <alberto.jhs.33@gmail.com>
2018-02-19 09:44:21 -0600

    Tratando de agregar carpeta de springboot

---
+++ albertojhs2/albertojhs/src/main/java/com/sixdelta/albertojhs/AlbertojhsApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.albertojhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbertojhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbertojhsApplication.class, args);
+	}
+}
