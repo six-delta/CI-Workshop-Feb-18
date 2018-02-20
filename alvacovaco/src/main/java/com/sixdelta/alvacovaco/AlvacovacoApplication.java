ee7514632597dd8bf3c2c7f063c0d4ede19c0e29
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-02-19 22:30:15 -0600

    Agregando mi proyecto spring

---
+++ alvacovaco/src/main/java/com/sixdelta/alvacovaco/AlvacovacoApplication.java
@@ -0,0 +1,12 @@
+package com.sixdelta.alvacovaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvacovacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvacovacoApplication.class, args);
+	}
+}
---
+++ alvacovaco/src/test/java/com/sixdelta/alvacovaco/AlvacovacoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixdelta.alvacovaco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlvacovacoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
