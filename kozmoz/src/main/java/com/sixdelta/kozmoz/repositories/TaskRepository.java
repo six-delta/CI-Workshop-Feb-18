dffb4ebacbd8fab9e85baaa250449370809e9c44
Enrique <kiqueyo847@gmail.com>
2018-02-20 13:42:04 -0600

    modificando repository

--- kozmoz/src/main/java/com/sixdelta/kozmoz/repositories/TaskRepository.java
+++ kozmoz/src/main/java/com/sixdelta/kozmoz/repositories/TaskRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.sixdelta.kozmoz.model.Task;
 
 public interface TaskRepository extends CrudRepository<Task, Integer>   {
-
+ 
+	public abstract int saveTask(int id);
 }
