93a8f44e1432e442c27d8de2db27b31748e984f8
svhector <nostgrand@gmail.com>
2018-02-20 12:06:00 -0600

    ¨Pegando carpeta de spring

---
+++ svHector/src/test/java/com/sixdelta/svHector/SvHectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixdelta.svHector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SvHectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
