15d90e4e5f2789e97ec286c24b6457d94fc07eff
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-02-19 22:23:46 -0600

    Un comit para subir datos

--- AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
+++ AlfonsoVACO_hollowbk201/src/main/java/com/sixdelta/app/StringCalculator.java
@@ -1,5 +1,8 @@
 package com.sixdelta.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 	public int add(String numbers) {
@@ -28,6 +31,7 @@ public class StringCalculator {
 	}
 
 	private int sumNumbersInStringArray(String[] numbersToBeInSum) {
+		convertArrayStringToArrayInt(numbersToBeInSum).forEach(i -> System.out.println(i));
 		int sumOfNumbersResult = 0;
 		int[] sumOfNumbers = new int[numbersToBeInSum.length];
 		for (int i = 0; i < numbersToBeInSum.length; i++)
@@ -35,6 +39,13 @@ public class StringCalculator {
 		for (int i : sumOfNumbers)
 			sumOfNumbersResult += i;
 		return sumOfNumbersResult;
+	}
+	
+	private List<Integer> convertArrayStringToArrayInt(String[] numeros) {
+		List<Integer> sumOfNumbers = new ArrayList<Integer>();
+		for (int i = 0; i < numeros.length; i++)
+			sumOfNumbers.add(convertToInt(numeros[i]));
+		return sumOfNumbers;
 	}
 
 }
