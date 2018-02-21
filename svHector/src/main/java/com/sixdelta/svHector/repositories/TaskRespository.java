f2fc7df1227d34635735587aea191263d31089e5
svhector <nostgrand@gmail.com>
2018-02-20 21:35:03 -0600

    Volviendo a construir archivos

--- svHector/src/main/java/com/sixdelta/svHector/repositories/TaskRespository.java
+++
@@ -1,9 +0,0 @@
-package com.sixdelta.svHector.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sixdelta.svHector.model.Task;
-
-public interface TaskRespository extends CrudRepository<Task, Integer> {
-
-}
