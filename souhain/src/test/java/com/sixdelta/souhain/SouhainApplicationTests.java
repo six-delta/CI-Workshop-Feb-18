50f86535fe5e88a13f1f97d625d424b2489b5fa2
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-21 17:03:11 -0600

    Deploy manual funcionando

---
+++ souhain/src/test/java/com/sixdelta/souhain/SouhainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixdelta.souhain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SouhainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
