9890e931cd8a8bcff3e8e2278bcad772b61b82f0
souhain <luis.vazquez.ch@hotmail.com>
2018-02-22 16:51:38 -0600

    cambiando de nombres

--- souhain/src/main/java/com/sixdelta/eddevelit/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sixdelta.eddevelit.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sixdelta.eddevelit.model.Task;
-
-public interface TaskRepository extends CrudRepository<Task, Integer>{
-
-}
